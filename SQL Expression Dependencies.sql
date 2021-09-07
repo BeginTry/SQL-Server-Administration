a30a2ebde6bf8949459a61f7a133ca9cc34723eb
Dave Mason <27367041+BeginTry@users.noreply.github.com>
2021-09-07 17:10:30 -0400

    SCHEMA_NAME( ) error
    Fix error: wrong parameter value was being passed to SCHEMA_NAME()

--- SQL Expression Dependencies.sql
+++ SQL Expression Dependencies.sql
@@ -2,6 +2,9 @@
 	This script iterates through all databases and finds dependency information for all objects.
 	It is particularly helpful with identifying cross-database and cross-server entities (among other things).
 */
+/*
+	Find object dependencies across all databases.
+*/
 IF EXISTS (
 	SELECT *
 	FROM tempdb.INFORMATION_SCHEMA.TABLES t
@@ -14,9 +17,9 @@ END
 GO
 
 SELECT 
-	DB_NAME() AS database_name,
-	SCHEMA_NAME(d.referencing_id) AS schema_name,
-	OBJECT_NAME(d.referencing_id) AS object_name, 
+	DB_NAME() AS database_name, 
+	SCHEMA_NAME(0) AS schema_name,
+	OBJECT_NAME(d.referencing_id, DB_ID()) AS object_name, 
 	d.referenced_server_name,
 	d.referenced_database_name,	
 	d.referenced_schema_name,	
@@ -25,12 +28,12 @@ INTO tempdb.guest.SqlExpressionDependencies
 FROM sys.sql_expression_dependencies d	
 WHERE 1 = 2;	
 		
-DECLARE @Cmd VARCHAR(1000) 
+DECLARE @Cmd VARCHAR(2000) 
 SELECT @Cmd = 'USE [?];
 
 INSERT INTO tempdb.guest.SqlExpressionDependencies
 SELECT 
-	DB_NAME() AS database_name,
+	DB_NAME() AS database_name, 	
 	SCHEMA_NAME(o.schema_id) AS schema_name,
 	OBJECT_NAME(d.referencing_id) AS object_name, 
 	d.referenced_server_name,
